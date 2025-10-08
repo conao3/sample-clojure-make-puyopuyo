3cc1eb39db2b5f3eda00ff4ddcd67f4df6087b6c
conao3 <conao3@gmail.com>
2025-10-09 02:04:34 +0900

    stage03

---
+++ sections/section02/.dir-locals.el
@@ -0,0 +1,8 @@
+((nil . ((cider-preferred-build-tool . clojure-cli)
+         (cider-clojure-cli-aliases . ":dev:nrepl")
+         (cider-default-cljs-repl . figwheel-main)
+         (cider-figwheel-main-default-options . "dev")
+         (cider-offer-to-open-cljs-app-in-browser . nil)
+
+         (cider-ns-refresh-before-fn . "com.stuartsierra.component.repl/stop")
+         (cider-ns-refresh-after-fn  . "com.stuartsierra.component.repl/start"))))
--- sections/section02/src/conao3/puyopuyo/cljs/app.cljs
+++ sections/section02/src/conao3/puyopuyo/cljs/app.cljs
@@ -1,4 +1,17 @@
 (ns conao3.puyopuyo.cljs.app)
 
+(def config
+  {:puyo-image-width 40
+   :puyo-image-height 40
+   :stage {:cols 6
+           :rows 12
+           :background-color "#11213b"}})
+
+(defn Stage []
+  [:div {:style {:width (str (* (-> config :puyo-image-width) (-> config :stage :cols)) "px")
+                 :height (str (* (-> config :puyo-image-height) (-> config :stage :rows)) "px")
+                 :background "url(sega-img/puyo_2bg.png)"
+                 :background-color (-> config :stage :background-color)}}])
+
 (defn App []
-  [:div "hello"])
+  [Stage])
