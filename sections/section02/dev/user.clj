3b4af7a94839f4bea6d31b42dc9e87df84a3ee69
conao3 <conao3@gmail.com>
2025-10-09 00:21:26 +0900

    add misc config

--- sections/section02/dev/user.clj
+++ sections/section02/dev/user.clj
@@ -1,1 +1,9 @@
-(ns user)
+(ns user
+  (:require
+   [clojure.java.classpath :as cp]
+   [clojure.string :as str]
+   [clojure.tools.namespace.repl :as clojure.tools.namespace.repl]))
+
+(->> (cp/classpath-directories)
+     (filter #(not (str/includes? % "resources")))
+     (apply clojure.tools.namespace.repl/set-refresh-dirs))
